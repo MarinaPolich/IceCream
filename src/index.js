ee6bce2fab7f35f1964406dcffc74937e7419f6b
ArtemBunin <108579146+ArtemBunin@users.noreply.github.com>
2022-08-28 19:07:42 +0300

    add image t animation for gallery

--- src/index.js
+++ src/index.js
@@ -4,6 +4,13 @@ $('.slider').slick({
     dots: true,
     arrows:false,
     slidesToShow: 1,
-
+responsive: [
+    {
+      breakpoint: 481,
+      settings: {
+            slidesToScroll: 2,
+               
+      },
+    },],
 
 });
