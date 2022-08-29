b8457ab0fd368e9166ddd61e0752c88c092e9cac
MarinaPolich <35706436+MarinaPolich@users.noreply.github.com>
2022-08-29 09:21:31 +0000

    Deploying to gh-pages from @ MarinaPolich/IceCream@5876357999df65c1968db9a0e913bfee8b8269ba 🚀

---
+++ index.0c587cf3.js
@@ -0,0 +1,2 @@
+$(".slider").slick({infinite:!1,dots:!0,arrows:!1,slidesToShow:1});
+//# sourceMappingURL=index.0c587cf3.js.map
--- index.7531dc16.js
+++
@@ -1,2 +0,0 @@
-$(".slider").slick({infinite:!1,dots:!0,arrows:!1,slidesToShow:1,responsive:[{breakpoint:480,settings:{slidesToScroll:2}}]});
-//# sourceMappingURL=index.7531dc16.js.map
